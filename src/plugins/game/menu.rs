ebc812815dd63a907044731aaebd7a8d4f481b33
theunrealtarik <58333332+theunrealtarik@users.noreply.github.com>
2024-05-24 23:35:26 +0100

    boo

--- src/plugins/game/menu.rs
+++ src/plugins/game/menu.rs
@@ -31,36 +31,38 @@ impl Plugin for GameMenuPlugin {
 struct Menu;
 
 impl GameMenuPlugin {
-    fn wait(mut commands: Commands, ui: Res<UiTextureAssets>) {
-        commands
-            .spawn((
-                NodeBundle {
-                    style: Style {
-                        width: Val::Percent(100.0),
-                        height: Val::Percent(100.0),
-                        justify_content: JustifyContent::Center,
-                        ..default()
-                    },
-                    ..default()
-                },
-                Menu,
-            ))
-            .with_children(|parent| {
-                parent.spawn((
+    fn wait(mut commands: Commands, ui: Res<UiTextureAssets>, query: Query<Entity, With<Menu>>) {
+        if query.is_empty() {
+            commands
+                .spawn((
                     NodeBundle {
                         style: Style {
-                            width: Val::Px(798.0 / 2.0),
-                            height: Val::Px(80.0 / 2.0),
-                            align_self: AlignSelf::Center,
-                            margin: UiRect::bottom(Val::Px(10.0)),
+                            width: Val::Percent(100.0),
+                            height: Val::Percent(100.0),
+                            justify_content: JustifyContent::Center,
                             ..default()
                         },
-                        background_color: Color::WHITE.into(),
                         ..default()
                     },
-                    UiImage::new(ui.pts.clone()),
-                ));
-            });
+                    Menu,
+                ))
+                .with_children(|parent| {
+                    parent.spawn((
+                        NodeBundle {
+                            style: Style {
+                                width: Val::Px(798.0 / 2.0),
+                                height: Val::Px(80.0 / 2.0),
+                                align_self: AlignSelf::Center,
+                                margin: UiRect::bottom(Val::Px(10.0)),
+                                ..default()
+                            },
+                            background_color: Color::WHITE.into(),
+                            ..default()
+                        },
+                        UiImage::new(ui.pts.clone()),
+                    ));
+                });
+        }
     }
 
     fn start(
